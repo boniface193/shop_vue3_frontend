d1868ffe0fe66c6c78720ac544ea4070eb161a60
Boniface Dennis <bonifacedennis40@yahoo.com>
2023-12-06 09:01:01 +0100

    vue 3 template

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,18 @@
 <template>
-<section>
-
-</section>
+  <RouterView></RouterView>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
-  components: {
-  }
-}
+  name: "App",
+  components: {},
+};
 </script>
 
-<style>
-
+<style lang="scss">
+@import url("@/styles/animate.css");
+@import url("@/styles/main.css");
+// @import url("@/styles/prettyPhoto.css");
+@import url("@/styles/price-range.css");
+@import url("@/styles/responsive.css");
 </style>
---
+++ src/views/index copy.vue
@@ -0,0 +1,77 @@
+<template>
+  <section>
+    <Headers />
+    <!-- headers -->
+    <section class="container">
+      <div class="row py-5">
+        <div class="col-sm-12">
+          <div class="item active">
+            <div class="col-sm-6">
+              <h1><span>E</span>-SHOPPER</h1>
+              <h2>Free E-Commerce Template</h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+              <button type="button" class="btn btn-default get">
+                Get it now
+              </button>
+            </div>
+            <div class="col-sm-6">
+              <img
+                src="@/assets/images/home/girl1.jpg"
+                class="girl img-responsive"
+                alt=""
+              />
+              <img
+                src="@/assets/images/home/pricing.png"
+                class="pricing"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section>
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-3">
+            <Category />
+            <!-- categories -->
+          </div>
+
+          <div class="col-sm-9 padding-right">
+            <FeaturedItems />
+            <!--features_items-->
+          </div>
+        </div>
+      </div>
+    </section>
+    <Footers />
+    <!-- footers -->
+  </section>
+</template>
+
+<script>
+import Headers from "@/layouts/header.vue";
+import Footers from "@/layouts/footer.vue";
+import Category from "@/components/category.vue";
+import FeaturedItems from "@/components/featuredItems.vue";
+export default {
+  name: "homePage",
+  components: {
+    Headers,
+    Footers,
+    Category,
+    FeaturedItems,
+  },
+};
+</script>
+
+<style lang="scss">
+.py-5 {
+	padding: 45px 0;
+}
+</style>
